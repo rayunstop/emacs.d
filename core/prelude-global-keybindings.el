d638595c2345c7209c48fbc756103b594174e2b5
yzprofile <yzprofiles@gmail.com>
2013-05-16 11:44:23 +0800

    Deleted ace-jump key bindings

--- core/prelude-global-keybindings.el
+++ core/prelude-global-keybindings.el
@@ -114,11 +114,11 @@
 ;; key chords
 (require 'key-chord)
 
-(key-chord-define-global "jj" 'ace-jump-word-mode)
-(key-chord-define-global "jl" 'ace-jump-line-mode)
-(key-chord-define-global "jk" 'ace-jump-char-mode)
+;(key-chord-define-global "jj" 'ace-jump-word-mode)
+;(key-chord-define-global "jl" 'ace-jump-line-mode)
+;(key-chord-define-global "jk" 'ace-jump-char-mode)
 (key-chord-define-global "JJ" 'prelude-switch-to-previous-buffer)
-(key-chord-define-global "uu" 'undo-tree-visualize)
+(key-chord-define-global "UU" 'undo-tree-visualize)
 
 (key-chord-mode +1)
 
