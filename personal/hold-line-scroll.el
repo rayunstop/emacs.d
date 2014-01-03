eb69e627dbdb136bdff3b127bbbd04589a6f8af3
yzprofile <yzprofiles@gmail.com>
2014-01-03 22:13:23 +0800

    split custorm.el to multi files

---
+++ personal/hold-line-scroll.el
@@ -0,0 +1,16 @@
+(defun hold-line-scroll-up()
+  "Scroll the page with the cursor in the same line"
+  (interactive)
+  ;; move the cursor also
+  (let ((tmp (current-column)))
+    (scroll-up 1)))
+
+(defun hold-line-scroll-down()
+  "Scroll the page with the cursor in the same line"
+  (interactive)
+  ;; move the cursor also
+  (let ((tmp (current-column)))
+    (scroll-down 1)))
+
+(global-set-key (kbd "M-n") 'hold-line-scroll-up)
+(global-set-key (kbd "M-p") 'hold-line-scroll-down)
