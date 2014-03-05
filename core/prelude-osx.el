225b09d86926fde7306b977364a666d813846889
yzprofile <yzprofiles@gmail.com>
2014-03-05 20:10:27 +0800

    Merge master to mine

--- core/prelude-osx.el
+++ core/prelude-osx.el
@@ -51,7 +51,7 @@ Windows external keyboard from time to time."
         (setq mac-command-modifier 'meta)
         (setq mac-option-modifier 'super)
         (message "Command is now bound to META and Option is bound to SUPER."))
-(progn
+    (progn
       (setq mac-command-modifier 'super)
       (setq mac-option-modifier 'meta)
       (message "Command is now bound to SUPER and Option is bound to META."))))
