183ef6a643f6a6a3627b05d9515379dfbeadedb9
yzprofile <yzprofiles@gmail.com>
2014-03-25 17:29:56 +0800

    update org-caldav.el

--- personal/org-google-cal.el
+++ personal/org-google-cal.el
@@ -3,9 +3,8 @@
 (require 'org-caldav)
 (setq org-caldav-url "https://www.google.com/calendar/dav")
 (setq org-caldav-calendar-id "yzprofiles@gmail.com")
-
-(setq org-caldav-inbox "~/Google Drive/org/caldav/caldav.org")
-(setq org-caldav-files org-agenda-files)
+(setq org-caldav-inbox '("~/Google Drive/org/caldav/caldav.org"))
+(setq org-caldav-files '("~/Google Drive/org/caldav/caldav.org"))
 (setq org-icalendar-date-time-format ";TZID=%Z:%Y%m%dT%H%M%S")
 
 (provide 'org-google-cal)
