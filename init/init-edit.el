4bdf8f3e800a5a47225156e66e646eed5fc53ef3
yzprofile <yzprofiles@gmail.com>
2014-10-19 21:11:48 +0800

    modify move-beginning-of-line

--- init/init-edit.el
+++ init/init-edit.el
@@ -17,4 +17,22 @@
 
 (show-paren-mode +1)
 
+(defun t-move-beginning-of-line (arg)
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+(global-set-key [remap move-beginning-of-line]
+                't-move-beginning-of-line)
+
+
 (provide 'init-edit)
