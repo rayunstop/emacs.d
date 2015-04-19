224c65ae045f0f02dd6a63f4dbd4e7132d6ea837
yzprofile <yzprofiles@gmail.com>
2015-04-20 00:58:48 +0800

    Fixed install company-go

--- init/init-go.el
+++ init/init-go.el
@@ -1,6 +1,6 @@
+(require-package 'go-mode)
 (require-package 'company)
 (require-package 'company-go)
-(require-package 'go-mode)
 (require-package 'go-eldoc)
 (require-package 'go-projectile)
 (require-package 'gotest)
