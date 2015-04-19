19f99d114b576bea7a9ce4150f5a9068e3abf5c2
yzprofile <yzprofiles@gmail.com>
2015-04-20 01:10:55 +0800

    move the note-path to custom file

--- init/init-deft.el
+++ init/init-deft.el
@@ -1,12 +1,12 @@
 (require-package 'deft)
 
-(setq icloud-path "~/Library/Mobile Documents/com~apple~CloudDocs/")
+(if (not note-path) (setq note-path "~/"))
 (setq deft-extension "org")
 (setq deft-text-mode 'org-mode)
-(setq deft-directory (concat icloud-path "Notes"))
+(setq deft-directory (concat note-path "Notes"))
 
 (defun gtd ()
   (interactive)
-  (helm-browse-project-find-files (concat icloud-path "GTD")))
+  (helm-browse-project-find-files (concat note-path "GTD")))
 
 (provide 'init-deft)
