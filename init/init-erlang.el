4a96e4fe08a6117f703268d54bd63651e7a392a5
yzprofile <yzprofiles@gmail.com>
2015-06-21 23:13:31 +0800

    delete snippets

--- init/init-erlang.el
+++ init/init-erlang.el
@@ -3,6 +3,7 @@
 
 
 (defun erlang-after-init-hook ()
+  (setq flycheck-erlang-include-path (list "../include/"))
   (setq erlang-root-dir "/usr/local/opt/erlang/lib/erlang")
   (setq exec-path (cons "/usr/local/bin" exec-path))
   (setq erlang-electric-commands '(erlang-electric-comma,
