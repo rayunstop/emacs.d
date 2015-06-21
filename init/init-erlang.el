f3bd994ce489d372cb6027f70872457af92da1c9
yzprofile <yzprofiles@gmail.com>
2015-06-22 02:36:36 +0800

    supported erlcscope

--- init/init-erlang.el
+++ init/init-erlang.el
@@ -2,15 +2,16 @@
 (require-package 'edts)
 
 
-(defun erlang-after-init-hook ()
+(defun erlang-after-load-hook ()
   (setq flycheck-erlang-include-path (list "../include/"))
   (setq erlang-root-dir "/usr/local/opt/erlang/lib/erlang")
   (setq exec-path (cons "/usr/local/bin" exec-path))
   (setq erlang-electric-commands '(erlang-electric-comma,
                                    erlang-electric-semicolon))
+  (cscope-minor-mode)
   (require 'erlang-start)
   (require 'edts-start))
 
-(add-hook 'after-init-hook 'erlang-after-init-hook)
+(add-hook 'erlang-mode-hook 'erlang-after-load-hook)
 
 (provide 'init-erlang)
--- init/init-helm.el
+++ init/init-helm.el
@@ -28,4 +28,9 @@
      (define-key company-mode-map (kbd "C-:") 'helm-company)
      (define-key company-active-map (kbd "C-:") 'helm-company)))
 
+(require-package 'helm-flycheck) ;; Not necessary if using ELPA package
+(eval-after-load 'flycheck
+  '(define-key flycheck-mode-map (kbd "C-c ! h") 'helm-flycheck))
+
+
 (provide 'init-helm)
